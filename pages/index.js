bd5af68c891ede328bc0bedd4252aff301bd7780
Mikhael Zimmermann <mikhael928@gmail.com>
2024-01-22 18:25:34 -0300

    Finish in construction page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Everyone is amazing</h1>;
+  return (
+    <div style={{height:'100vh',width:'100%', display:'flex',justifyContent:'center',alignItems:'center'}}>
+      <h1>Project in construction</h1>
+    </div>
+  );
 }
 
 export default Home;
