5aec8bca879802c78f87d6d04bd0b91a3582da5d
mikhael928 <mikhael928@gmail.com>
2024-01-23 13:53:11 +0000

    Creating endpoint /status and the request test

--- models/calculator.js
+++
@@ -1,7 +0,0 @@
-function sum(numero01, numero02) {
-  if (typeof numero01 !== "number" || typeof numero02 !== "number")
-    return "Erro";
-  return numero01 + numero02;
-}
-
-exports.sum = sum;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div style={{height:'100vh',width:'100%', display:'flex',justifyContent:'center',alignItems:'center'}}>
-      <h1>Project in construction</h1>
+      <h1>Project in construction</h1>      
     </div>
   );
 }
