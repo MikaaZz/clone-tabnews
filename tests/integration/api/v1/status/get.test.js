a78648e70e9bee92b0237ac66236eaab32b56aea
Mikhael Zimmermann <mikhael928@gmail.com>
2024-01-24 21:36:31 -0300

    Adding .env variables and create database

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,4 @@
-const { exportTraceState } = require("next/dist/trace");
-
-test("GET to /api/v1/status should return 200", async () => { 
-    const response = await fetch("https://3000-monospace-tecnology-news-1706008415363.cluster-nxnw2gov3naqkvuxb437f67u5e.cloudworkstations.dev/api/v1/status");
-    expect(response.status).toBe(200);
-})
+test("GET to /api/v1/status should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  expect(response.status).toBe(200);
+});
